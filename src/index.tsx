862f312561d76adc7596688e3eb6224678a01f5e
Jhonatan <jhonatan.saman@gok.digital>
2022-01-05 09:04:43 -0300

    feat: transformJsonToRFC function

--- src/index.tsx
+++ src/index.tsx
@@ -6,5 +6,30 @@ interface Props {
 }
 
 export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return <div className={styles.test}>{text}</div>
+}
+
+export const DefaultImage = ({ src }: { src: string }) => {
+  return (
+    <div>
+      <img src={src} style={{ width: '100%', height: '100%' }} />
+    </div>
+  )
+}
+
+const rFCStringMapper = {
+  ExampleComponent: ExampleComponent,
+  DefaultImage: DefaultImage
+}
+
+export const transformJsonToRFC = (
+  json: { name: string; props: any }[]
+): React.ReactElement[] => {
+  const Components = json.map(item => {
+    return React.createElement(rFCStringMapper[item.name], {
+      ...item.props
+    })
+  })
+
+  return Components
 }
